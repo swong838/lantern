bb842ec55a6a001bb73750dbab811121ac058f1a
swong838 <sw.advocate@gmail.com>
2015-05-05 22:45:05 -0700

    just futzing
    totally futzing around with some base setup ideas, nothing's even
    slightly tested or even syntax checked

--- lantern.ino
+++ lantern.ino
@@ -1,9 +1,45 @@
+#include <Adafruit_NeoPixel.h>
+
+/* LED info */
+#define NUMPIXELS   2
+#define RED         200
+#define GREEN       145
+#define BLUE        0
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+/* chip IO */
+#define PIXEL_PIN   1
+#define BUTTON_PIN  2
+
+/* time */
+#define RUNTIME     14400000
+unsigned long       startTime;
+
+
 void setup() {
-  // put your setup code here, to run once:
+    // trinket setup
+    #if defined (__AVR_ATtiny85__)
+      if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+    #endif
+
+    // init neopixel library
+    pixels.begin(); 
+    startTime = millis();
+}
+
+void updatePixels() {
+    for(int i=0;i<NUMPIXELS;i++){
+        // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+        pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+        pixels.show();
+    }
 
 }
 
+void getBaseLuminance(
+}
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
